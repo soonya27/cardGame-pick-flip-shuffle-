b8f39e21ce30b621525f5abceecc1c24405ec28a
soyeong <psykor48@gmail.com>
2023-09-01 16:43:20 +0900

    랜덤이미지추가, animation.click 배열 작업

--- card_suffle.js
+++ card_suffle.js
@@ -79,7 +79,11 @@ class TaroCard {
                 if (!e.target.closest('li').classList.contains('unClick')) {
                     return;
                 }
+                if (this.animation.click && this.animation.click.playState == 'running') {
+                    return;
+                }
                 this.#clickAnimation(e);
+
 
                 //animation끝난후 clickable class추가로 수정 -> 아래코드 주석
                 // if (this.#checkRunningAnimation().length == 0) {
@@ -184,6 +188,28 @@ class TaroCard {
         //class삭제
         this.cardField.querySelector('ul').classList.remove('clickable');
 
+
+        //카드 뽑힌게 있으면 -> 
+        //카드 뽑는중이면
+        if (this.animation.click && this.animation.click.some(item => item.playState == 'running')) {
+            console.log(this.animation.click)
+            this.#stopAnimation();
+            // this.animation.click[this.animation.click.length - 1].onfinish = () => {
+            this.resetFlip();
+            this.animation.flipBack.onfinish = () => {
+                this.cardField.querySelectorAll('li img.back').forEach(item => {
+                    item.remove();
+                });
+                this.playShuffleAnimation();
+            }
+            // }
+
+        } else {
+            this.playShuffleAnimation();
+        }
+    }
+
+    playShuffleAnimation() {
         this.#stopAnimation('spread', 'shuffle');
 
         if (this.animation.spread.playState != 'finished') {
@@ -328,7 +354,7 @@ class TaroCard {
     #checkRunningAnimation() {
         const result = [];
         for (const key in this.animation) {
-            (this.animation[key].playState == 'running') && result.push(this.animation[key]);
+            (this.animation[key].playState == 'running') && result.push({ name: key, animation: this.animation[key] });
         }
         return result;
     }
@@ -345,62 +371,110 @@ class TaroCard {
         target.classList.replace('unClick', 'clicked');
 
 
-        //뽑히는 motion
-        target.animate([
-            { transform: 'translateY(-35%)' },
-        ],
-            { duration: 600, fill: "backwards" });
-        target.animate([
-            { opacity: 0 },
-        ],
-            { duration: 600, delay: 300, fill: "backwards" });
+        //서버이미지 임시------------ 이미지 로딩추가 ....
+        const img = new Image();
+        img.src = '/cover.png';
+        img.onload = () => {
 
-
-        //정중앙에서 flip  
-        this.animation.center = target.animate([
-            { top: '50%', left: '50%', transform: 'translate(-50%,-50%)', opacity: 0 },
-            { top: '50%', left: '50%', transform: 'translate(-50%,-50%) scale(2)', opacity: 0 },
-            { top: '50%', left: '50%', transform: 'translate(-50%,-50%) scale(2)', opacity: 1, zIndex: 9999 },
-        ],
-            { duration: 1200, delay: 900, fill: "forwards" });
-
-        //back 뒷면카드 요소 추가
-        this.animation.center.onfinish = () => {
             const back = document.createElement('img');
             const imgUrl = this.cardList.find(item => item.data.id == target.getAttribute('data-id')).data.imgUrl;
             back.classList.add('back');
             back.setAttribute('src', imgUrl);
 
+            //뽑는 motion
+            this.animation.click = [];
+            const pickMotion01 = e.target.parentNode.animate([
+                { transform: 'translateY(-35%)' },
+            ],
+                { duration: 600, fill: "forwards" });
+            const pickMotion02 = e.target.parentNode.animate([
+                { opacity: 0 },
+            ],
+                { duration: 600, delay: 300, fill: "forwards" });
+            this.animation.click.push(pickMotion01)
+            this.animation.click.push(pickMotion02)
 
-            back.style.transform = 'rotateY(180deg)';
-            target.appendChild(back);
+            //뽑히는동안은 다른카드 pick막기
+            e.target.closest('ul').classList.remove('clickable');
 
-            target.querySelector('img.front').animate({
+            //정중앙에서 flip  
+            const filpMotion = target.animate([
+                { top: '50%', left: '50%', transform: 'translate(-50%,-50%)', opacity: 0 },
+                { top: '50%', left: '50%', transform: 'translate(-50%,-50%) scale(2)', opacity: 0 },
+                { top: '50%', left: '50%', transform: 'translate(-50%,-50%) scale(2)', opacity: 1, zIndex: 9999 },
+            ],
+                { duration: 1200, delay: 900, fill: "forwards" });
+            this.animation.click.push(filpMotion)
+            //back 뒷면카드 요소 추가
+            filpMotion.onfinish = () => {
+
+                back.style.transform = 'rotateY(180deg)';
+                target.appendChild(back);
+
+                //front 뒤집기
+                const flipBack01 = target.querySelector('img.front').animate({
+                    transform: 'rotateY( 180deg )',
+                },
+                    { duration: 600, fill: "forwards" });
+
+                const flipBack02 = back.animate({
+                    transform: 'rotateY( 0deg )',
+                },
+                    { duration: 600, fill: "forwards" });
+                this.animation.click.push(flipBack01)
+                this.animation.click.push(flipBack02)
+
+                //하단영역 중앙정렬
+                const selectedCardPositionList = [
+                    [{ top: this.selectedAreaPosition.verticleCenter, left: '50%' }],
+                    [{ top: this.selectedAreaPosition.verticleCenter, left: '30%' }, { top: this.selectedAreaPosition.verticleCenter, left: '60%' }],
+                    [{ top: 0, left: 0 }, { top: 0, left: 0 }, { top: 0, left: 0 }],
+                    [{ top: 0, left: 0 }, { top: 0, left: 0 }, { top: 0, left: 0 }, { top: 0, left: 0 }]
+                ];
+                const positionCenter = target.animate([
+                    {
+                        top: this.selectedAreaPosition.verticleCenter + 'px',
+                        left: '50%',
+                        transform: 'translate(-50%, -50%)',
+                        zIndex: 1
+                    },
+                ],
+                    { duration: 600, delay: 1200, fill: "forwards" });
+
+                this.animation.click.push(positionCenter)
+
+                positionCenter.onfinish = () => {
+                    e.target.closest('ul').classList.add('clickable');
+                }
+            }
+
+
+        };
+
+    }
+
+
+    resetFlip() {
+        //shuffle -> flip 카드 reset
+        //back요소 삭제,
+        //front 뒤집기
+        //clicked -> unClick
+        this.cardField.querySelectorAll('li.clicked').forEach(item => {
+            item.classList.replace('clicked', 'unClick');
+            // item.querySelector('img.front').style.transform = 'rotateY(0deg )';
+
+            (item.querySelector('img.back'))
+            //반대로 뒤집기  front->앞 back->뒤
+            item.querySelector('img.front').animate({
+                transform: 'rotateY(0deg)',
+            },
+                { duration: 600, fill: "forwards" });
+            this.animation.flipBack = item.querySelector('img.back').animate({
                 transform: 'rotateY( 180deg )',
             },
                 { duration: 600, fill: "forwards" });
 
-            back.animate({
-                transform: 'rotateY( 0deg )',
-            },
-                { duration: 600, fill: "forwards" });
-
-            //하단영역 중앙정렬
-            target.animate([
-                { opacity: 0, top: this.selectedAreaPosition.top + 'px' },
-                {
-                    top: this.selectedAreaPosition.verticleCenter + 'px',
-                    transform: 'translate(-50%, -50%)',
-                    zIndex: 1
-                },
-            ],
-                { duration: 600, delay: 1200, fill: "forwards" });
-        }
-
-
-
-
-
+        });
 
     }
 
